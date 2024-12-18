7b0021dab7e3cf482c3f0144575031baaf74430a
TanakornWJ <wangjan2548@gmail.com>
2024-12-18 09:57:05 +0700

    LAB401

--- LABSHEET04/src/LAB401.java
+++ LABSHEET04/src/LAB401.java
@@ -1,41 +1,65 @@
 import javax.swing.*;
-public class LAB401 
-{
-
-	public static void main(String[] args) 
-	{
+public class LAB401 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		inputEmail();
+		
 		
 	}
 	public static void inputEmail() {
 		String ans;
 		String varEmail;
+		
 		do {
-			varEmail = JOptionPane.showInputDialog("Input ur e-mail");
-			if(varEmail !=null) {
+			varEmail = JOptionPane.showInputDialog("Input your e-mail:");
+			
+			if (varEmail != null && !varEmail.isEmpty()) {
 				checkEmailError(varEmail);
-			}
-			else {
+				
+			} else {
 				JOptionPane.showMessageDialog(null, "E-mail cannot be empty!!!");
-				
 			}
-			ans = JOptionPane.showInputDialog("Do you want to input email address [Y/y]");
+			
+			ans = JOptionPane.showInputDialog("Do you wnt to input email ddress[Y/y]");
+			
 		}while (ans != null && ans.equalsIgnoreCase("y"));
+		
 	}
-		public static void checkEmailError(String email) 
-		{
-			while(email.startsWith("@")|| email.contains("")) {
-				email = JOptionPane.showInputDialog("Invalid e-mail. INput your e-mail again:");
-				
-			}
-			email = email.toLowerCase();
-			JOptionPane.showMessageDialog(null,checkEmail(email));
-		}
-		public static boolean checkEmail(String email) {
-			if (email.endsWith("@gmail.com")|| email.endsWith("@hotmail.com")) {
-				return true;
-			}
-			else return false;
-		}
 	
+	// checkEmailError() Method
+	
+	public static void checkEmailError(String email) {
+		
+		while (email.startsWith("@") || email.contains(" ")) {
+			email = JOptionPane.showInputDialog("Invail e-mail. Input your e-mail again:");
+		}
+		
+		email = email.toLowerCase();
+		
+		JOptionPane.showMessageDialog(null, checkEmail(email)
+				?"Your e-mail is " + email // return true
+				:"Your e-mail is not a hotmail or gnail address." // return false
+				);
+	}
+	
+	public static boolean checkEmail(String email) {
+		//1
+		
+		/*if (email.endsWith("@gmail.com") || email.endsWith("@hotmail.com")) {
+			return true;
+		} else {
+			return false;
+		}*/ 
+		
+		//2
+		
+		/*
+		if (email.endsWith("@gmail.com") || email.endsWith("@hotmail.com")) {
+			return true;
+		}
+		return false;
+		*/
+		
+		return email.endsWith("@gmail.com") || email.endsWith("@hotmail.com");
+	}
 }
