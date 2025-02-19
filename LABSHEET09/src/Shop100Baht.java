1fe07646ae5f89f68f6ef6517435a235ad0a82be
TanakornWJ <wangjan2548@gmail.com>
2025-02-19 09:30:50 +0700

    Lab09

--- LABSHEET09/src/Shop100Baht.java
+++ LABSHEET09/src/Shop100Baht.java
@@ -5,21 +5,19 @@ public class Shop100Baht {
     public static void main(String[] args) {
         Product sold = new Product();
 
-        //String input = JOptionPane.showInputDialog("Input the number of product: ");
-        //int number = Integer.parseInt(input); // Convert String to int
+        
 
         int response = JOptionPane.showConfirmDialog(null, "Is this program run on Pattanakarn Branch?", "Branch Selection", JOptionPane.YES_NO_OPTION);
         if (response == JOptionPane.YES_OPTION) {
-            sold = new PattanakarnBranch(); // ใช้คลาส PattanakarnBranch ถ้ากด Yes
+            sold = new PattanakarnBranch(); 
         } else {
-            sold = new Product(); // ใช้คลาส Product ถ้ากด No
+            sold = new Product(); 
         }
 
         int number = Integer.parseInt(JOptionPane.showInputDialog("Input the number of product: "));
         sold.setUnit(number);
 
-        //String massage = sold.toString();
-        //JOptionPane.showMessageDialog(null, massage);
+        
         JOptionPane.showMessageDialog(null, sold);
 
     }
